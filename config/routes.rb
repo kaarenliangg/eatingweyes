3b53f54da9c9327aa6ff9b07e2915fae0d358cdb
kaarenliangg <karenliang9546@gmail.com>
2023-04-28 10:56:40 +1000

    minor updates

--- config/routes.rb
+++ config/routes.rb
@@ -4,28 +4,12 @@ Rails.application.routes.draw do
   resources :categories
   resources :recipes
   resources :sessions
+ 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy'
 
-  
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/show'
-  get 'categories/destroy'
-  get 'recipes/index'
-  get 'recipes/new'
-  get 'recipes/create'
-  get 'recipes/edit'
-  get 'recipes/update'
-  get 'recipes/show'
-  get 'recipes/destroy'
-  get 'session/new'
-  get 'session/create'
-  get 'session/destroy'
+
   get 'users/index'
   get 'users/new'
   get 'users/create'
